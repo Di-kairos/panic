bbe261235067fa69feb31bcc483baaff823ec44b
Di-kairos <205350801+Di-kairos@users.noreply.github.com>
2026-08-03 22:50:18 +0300

    chore(formula): bump to v0.1.9

--- Formula/panic.rb
+++ Formula/panic.rb
@@ -2,7 +2,7 @@ class Panic < Formula
   desc "One-step hide-and-lock kill-switch for macOS"
   homepage "https://github.com/Di-kairos/panic"
   url "https://github.com/Di-kairos/panic/archive/refs/tags/v0.1.9.tar.gz"
-  sha256 "51bbc2cd2488fe8c46715857170c177258c1208821a419348990f8992a097574"
+  sha256 "dd58438fa3af616ae7944e281848b349d6824c17dc68b73c0d28e5af1162b7ea"
   license "MIT"
 
   def install
